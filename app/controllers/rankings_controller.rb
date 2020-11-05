664a2a482cb9479a9781ed86a833378196fac189
MasahiroYamamoto <m-yamamoto@ase.co.jp>
2020-11-05 09:07:12 +0900

    第2ソート指定

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -9,19 +9,19 @@ class RankingsController < ApplicationController
 
     case params[:sorted_by] 
     when "win_rate" then
-      @cards = @cards.order(win_rate: "DESC") 
+      @cards = @cards.order(win_rate: "DESC", number: "ASC") 
     when "use_rate" then
-      @cards = @cards.order(use_rate: "DESC") 
+      @cards = @cards.order(use_rate: "DESC", number: "ASC") 
     when "score_ave" then
-      @cards = @cards.order(score_ave: "DESC") 
+      @cards = @cards.order(score_ave: "DESC", number: "ASC") 
     when "pick_ave" then
-      @cards = @cards.order(pick_ave: "DESC") 
+      @cards = @cards.order(pick_ave: "DESC", number: "ASC") 
     when "number" then
       @cards = @cards.order(number: "ASC") 
     when "name" then
       @cards = @cards.order(name_ja: "ASC") 
     else
-      @cards = @cards.order(win_rate: "DESC") 
+      @cards = @cards.order(win_rate: "DESC", number: "ASC") 
     end
     @cards
   end
