f47c4dd7c51fc1808f03f63be3c4f07fbe8297f5
martha <hellomartha0@gmail.com>
2020-11-13 17:29:40 +0900

    ランキングオーダー変更

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -9,21 +9,21 @@ class RankingsController < ApplicationController
 
     case params[:sorted_by] 
     when "win_rate" then
-      @cards = @cards.order(win_rate: "DESC", number: "ASC") 
+      @cards = @cards.order(win_rate: "DESC", num_of_pick: "DESC", number: "ASC") 
     when "use_rate" then
-      @cards = @cards.order(use_rate: "DESC", number: "ASC") 
+      @cards = @cards.order(use_rate: "DESC", num_of_pick: "DESC", number: "ASC") 
     when "score_ave" then
-      @cards = @cards.order(score_ave: "DESC", number: "ASC") 
+      @cards = @cards.order(score_ave: "DESC", num_of_pick: "DESC", number: "ASC") 
     when "pick_ave" then
-      @cards = @cards.order(pick_ave: "ASC", number: "ASC") 
+      @cards = @cards.order(pick_ave: "ASC", num_of_pick: "DESC", number: "ASC") 
     when "number" then
       @cards = @cards.order(number: "ASC") 
     when "name" then
-      @cards = @cards.order(name_ja: "ASC", name_en: "ASC") 
+      @cards = @cards.order(name_ja: "ASC", name_en: "ASC", number: "ASC") 
     when "num_of_pick" then
       @cards = @cards.order(num_of_pick: "DESC") 
     else
-      @cards = @cards.order(win_rate: "DESC", number: "ASC") 
+      @cards = @cards.order(win_rate: "DESC", num_of_pick: "DESC", number: "ASC") 
     end
     @cards
   end
