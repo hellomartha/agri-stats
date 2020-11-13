89395eaedceede65ea2271d7d77e455d2686e17f
martha <hellomartha0@gmail.com>
2020-11-13 17:43:56 +0900

    page not found

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :render_404
+
   def model
     raise NotImplementedError.new("You must implement #{self.class}##{__method__}")
   end
 
   def columns_of_show
     model.attribute_names
+  end
+
+  def render_404
+    render template: 'application/page_not_found'
   end
 end
